ccaedd3a50e022434aab4216dcce275dbf1763f1
Ruben Romero <rubenxrb@gmail.com>
2021-09-06 14:23:47 -0700

    Corrección de parámetros

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -40,10 +40,11 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: RaisedButton(
             onPressed: () async {
+              const String mpPreferenceId = "251601260-25272df5-b188-436f-a9a0-29c31d871301";
+              const String mpPublicKey = "APP_USR-4a4313ac-bede-4a66-b43d-25b9ec036783";
               (await MercadoPagoIntegration.startCheckout(
-                publicKey: "PUBLIC_KEY",
-                preference: preference,
-                accessToken: "ACCESS_TOKEN",
+                publicKey: mpPublicKey,
+                preferenceId: mpPreferenceId
               ))
                   .fold((Failure failure) {
                 debugPrint('Failure => ${failure.message}');
